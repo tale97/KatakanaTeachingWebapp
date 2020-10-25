b8c00ad597a6e74a9fd3a7bd9011d0af54bbf48f
tale97 <ttle@college.harvard.edu>
2020-10-25 17:33:20 -0400

    switch to word-based version

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ const store = createStore(
 ReactDOM.render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <App2 />
+      <App />
     </ThemeProvider>
   </Provider>,
   document.getElementById("root")
